ac2c94f475c8438ac4a15537800a7f13923cdafa
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-08-09 05:40:43 -0400

    Automatic save commit initiated at 2021-08-09_05-40-43

--- check_git.py
+++ check_git.py
@@ -4,5 +4,5 @@
 
 
 def get_last_commit():
-    file = open('/.git/COMMIT_EDITMSG', 'r')
+    file = open('/home/scratchbowling/Scratch-Bowling-Series-Website/.git/COMMIT_EDITMSG', 'r')
     print(file.read())
