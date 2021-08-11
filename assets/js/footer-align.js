a8297152ec464b1a733fc1f54585935d96aeaaea
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-08-11 06:46:48 -0400

    Automatic save commit initiated at 2021-08-11_06-46-48

--- assets/js/footer-align.js
+++ assets/js/footer-align.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
             $('.nav-account').css('display', 'flex');
             $('.nav-buttons').css('display', 'block');
             $('.nav-drop-button i').css('text-shadow', '-1px -1px 0 #d9af62,  1px -1px 0 #d9af62,-1px 1px 0 #d9af62,1px 1px 0 #d9af62');
-            $('#nav-icon1').addClass('open');
+            $('#nav-icon3').toggleClass('open');
         }
         else
         {
@@ -21,7 +21,7 @@ $(document).ready(function() {
             $('.nav-account').css('display', 'none');
             $('.nav-buttons').css('display', 'none');
             $('.nav-drop-button i').css('text-shadow', 'none');
-            $('#nav-icon1').removeClass('open');
+            $('#nav-icon3').toggleClass('open');
         }
     });
     var msg_closed = true;
