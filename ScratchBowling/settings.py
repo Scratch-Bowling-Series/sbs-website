a4aa4c86f4a76e08a78c373f1296973eed511d0f
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-08-11 23:24:51 -0400

    Automatic save commit initiated at 2021-08-11_23-24-51

--- ScratchBowling/settings.py
+++ ScratchBowling/settings.py
@@ -88,17 +88,17 @@ WSGI_APPLICATION = 'ScratchBowling.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    #   'default': {
-    #   'ENGINE': 'django.db.backends.mysql',
-    #   'NAME': 'scratchbowling$default',
-    #   'USER': 'scratchbowling',
-    #   'PASSWORD': 'dvSdd260*HXp',
-    #   'HOST': 'scratchbowling.mysql.pythonanywhere-services.com',
-    #}
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+       'default': {
+       'ENGINE': 'django.db.backends.mysql',
+       'NAME': 'scratchbowling$default',
+       'USER': 'scratchbowling',
+       'PASSWORD': 'dvSdd260*HXp',
+       'HOST': 'scratchbowling.mysql.pythonanywhere-services.com',
     }
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
 
 
