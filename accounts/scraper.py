0b820a662e8020fe658572f54aa141df160deab7
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-08-18 20:22:28 -0400

    Fixes

--- accounts/scraper.py
+++ accounts/scraper.py
@@ -10,7 +10,7 @@ from tournaments.tournament_scraper import convert_abr
 User = get_user_model()
 
 def UpdateUsers():
-    urls = get_account_urls(40)
+    urls = get_account_urls(5)
     bowlers = []
     count = 0
     waittime = 0
@@ -73,7 +73,7 @@ def UpdateUsers():
 
 def get_account_urls(pages):
     urls = []
-    for x in range(0, pages):
+    for x in range(35 - pages, 35):
         with urlopen('http://www.scratchbowling.com/bowler-bios?page=' + str(x)) as response:
             print('Scraping Bowler Page: (' + str(x) + '/' + str(pages) + ')')
             soup = BeautifulSoup(response, 'lxml')
