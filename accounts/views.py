d528fb361ee35c0e14451866de6e8b4e0dcac9e6
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-08-20 07:29:23 -0400

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -103,9 +103,6 @@ def accounts_modify_view(request):
 
 
 def accounts_login_view(request):
-    email = EmailMessage('Subject', 'Body', to=['cstarrfish27@gmail.com'], from_email='christianjstarr@icloud.com')
-    email.send()
-    print('Sent Email')
     if request.method == 'POST':
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
