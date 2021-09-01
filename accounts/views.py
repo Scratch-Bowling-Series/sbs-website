d01e21ca5133111b495ba62c3a293e55635179f0
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-09-01 13:41:50 -0400

    Automatic save commit initiated at 2021-09-01_13-41-50

--- accounts/views.py
+++ accounts/views.py
@@ -227,8 +227,7 @@ def accounts_socialcard_image(request, id):
 
         profile_pic.thumbnail(profile_pic_size)
 
-        card_pic.paste(profile_pic, (500, 500))
-        card_pic = profile_pic
+        card_pic.paste(profile_pic, (100, 100))
         response = HttpResponse(content_type='image/jpg')
         card_pic.save(response, "PNG")
         return response
