347ffe4a329512e3196f3923eed879c9d29b8975
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-09-01 13:33:35 -0400

    Automatic save commit initiated at 2021-09-01_13-33-34

--- accounts/views.py
+++ accounts/views.py
@@ -223,7 +223,7 @@ def accounts_socialcard_image(request, id):
         profile_pic = open('/home/scratchbowling/Scratch-Bowling-Series-Website/media/' + str(user.picture), 'rb')
         profile_pic_size = (250, 250)
 
-        card_pic = open('/home/scratchbowling/Scratch-Bowling-Series-Website/static/img/social-card-template.png', 'rb')
+        card_pic = open('/home/scratchbowling/Scratch-Bowling-Series-Website/assets/img/social-card-template.png', 'rb')
 
         profile_pic.thumbnail(profile_pic_size)
 
