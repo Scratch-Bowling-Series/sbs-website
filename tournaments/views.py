63b6c74c17ab6f5235b8f3b6586ba929282d0fc8
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-08-31 21:41:12 -0400

    Automatic save commit initiated at 2021-08-31_21-41-12

--- tournaments/views.py
+++ tournaments/views.py
@@ -319,8 +319,8 @@ def tournaments_results_views(request, page=1):
     per_page = 20
     selected_upcoming = False
     tournaments_count = Tournament.objects.all().count()
-    upcoming_count = Tournament.objects.filter(tournament_date__gte=datetime.now().date()).exclude(tournament_date=datetime.now().date(), tournament_time__lt=datetime.now().time()).count()
     tournaments_past = Tournament.objects.filter(tournament_date__lte=datetime.now().date()).exclude(tournament_date=datetime.now().date(), tournament_time__gt=datetime.now().time())
+    results_count = len(tournaments_past)
     if request.method == 'POST':
         form = TournamentsSearch(request.POST)
         if form.is_valid():
@@ -342,7 +342,7 @@ def tournaments_results_views(request, page=1):
                                                                  'tournaments_past': tournaments_past,
                                                                  'selected_upcoming':selected_upcoming,
                                                                  'tournaments_count': tournaments_count,
-                                                                 'upcoming_count': upcoming_count,
+                                                                 'results_count': results_count,
                                                                  'search_type': 'tournaments_results',
                                                                  'search': search,
                                                                  'page': create_page_obj(page, per_page, total_count)
