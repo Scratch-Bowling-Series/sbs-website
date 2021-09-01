59a8babae9434ae57fc9276606a5f5edcc29057b
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-09-01 14:44:31 -0400

    Automatic save commit initiated at 2021-09-01_14-44-31

--- accounts/views.py
+++ accounts/views.py
@@ -234,7 +234,7 @@ def accounts_socialcard_image(request, id):
 
 
 def create_profile_pic_circle(profile_pic, profile_pic_size):
-    profile_pic.thumbnail(profile_pic_size)
+    profile_pic.crop(profile_pic_size)
     alpha_mask = Image.new("L", profile_pic_size, 0)
     draw = ImageDraw.Draw(alpha_mask)
     draw.ellipse([(0, 0), profile_pic_size], fill=255)
