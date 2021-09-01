37e63bb8dc49a0c1d63c8607754657e416d52a14
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-09-01 14:06:27 -0400

    Automatic save commit initiated at 2021-09-01_14-06-27

--- accounts/views.py
+++ accounts/views.py
@@ -233,8 +233,7 @@ def accounts_socialcard_image(request, id):
         shape = [(0, 0), (width, height)]
         draw.ellipse(shape, fill=0)
         profile_pic.putalpha(mask_pic)
-        card_pic = Image.composite(card_pic, profile_pic)
-        ##card_pic.paste(profile_pic, (100, 100), mask_pic)
+        card_pic.paste(profile_pic, (100, 100))
         response = HttpResponse(content_type='image/jpg')
         card_pic.save(response, "PNG")
         return response
