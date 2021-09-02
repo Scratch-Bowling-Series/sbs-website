1de76968420200cd52cdb58ed59951b8fd184ce9
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-09-01 22:36:29 -0400

    Automatic save commit initiated at 2021-09-01_22-36-29

--- assets/js/share.js
+++ assets/js/share.js
@@ -20,6 +20,24 @@ $(document).ready(function(){
        });
    });
 
+   $('.share-page-direct').click(function(){
+      var pageUrl = $('.share-page-direct').attr('data');
+      pageUrl = pageUrl.replace('https://scratchbowling.pythonanywhere.com', '');
+      pageUrl = pageUrl.replaceAll('/', '&sl');
+       $.ajax({
+           type: "GET",
+           url: "https://scratchbowling.pythonanywhere.com/s/create/new/" + pageUrl,
+           contentType: "text/plain",
+           dataType: "text",
+           success: function (data) {
+               OpenShareBox();
+               $('.share-link').html(data.toString().replace('.pythonanywhere', '') + '<span id="link-notify" class="link-copy-notify">LINK COPIED TO CLIPBOARD</span>');
+               $('.share-link').click(function (){ CopyTo(data.toString()); });
+               $('.link-copy').click(function (){ CopyTo(data.toString()); });
+           }
+       });
+   });
+
    function OpenShareBox(){
         $('.sharebox').css('display', 'block');
         $('.link-copy-notify').css('opacity', 0);
