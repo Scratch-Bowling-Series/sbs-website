0122af49c89dd413ffae059fff822a15bd30d448
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-09-04 13:51:16 -0400

    Update popup.js

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,30 +1,32 @@
 $(document).ready(function()
 {
     $('.popup').show();
-    $('.popup .close').click(function ()
-        {
-            $('.popup').hide();
-        });
-        $('.popup .continue').click(function ()
-        {
-            $('.popup').hide();
-            var url = $('.popup .continue').attr('data');
-            if(url != ''){
-                setTimeout(function (){ window.location.href = url; }, 1000);
-            }
-        });
+    $('.popup .close').click(function (){
+        $('.popup').hide();
+    });
+    $('.popup .continue').click(function (){
+        $('.popup').hide();
+        var url = $('.popup .continue').attr('data');
+        if(url != ''){
+            setTimeout(function (){ window.location.href = url; }, 1000);
+        }
+    });
 
-    CenterPopup();
     $(window).resize(function(){
 
         CenterPopup();
     });
 
+    CenterPopup();
+
+
     function CenterPopup(){
-            if($('.popup-inner').height() > $('.popup').height()){
-                $('.popup-inner').height($('.popup').height());
+            var inheight = $('.popup-inner').height();
+
+            if(inheight > $(window).height()){
+                $('.popup-inner').height($(window).height());
             }
-            var margins = ($('.popup').height() - $('.popup-inner').height()) / 2;
+            var margins = ($(window).height() - inheight) / 2;
             $('.popup-inner').css('margin-top', margins);
             $('.popup-inner').css('margin-bottom', margins);
             console.log('applied margin ' + margins);
