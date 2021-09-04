0eff2a6268d1cf7d5866cedc770fc8520b551142
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-09-04 12:07:20 -0400

    Signed-off-by: ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>

--- ScratchBowling/popup.py
+++ ScratchBowling/popup.py
@@ -3,17 +3,52 @@ from django.db.models import Q
 from accounts.account_helper import get_location_basic_obj, User
 
 
+class Popup:
+    popup_id = 0
+    data = 0
+    data_length = 0
+
+def cut_last_name(users, last_name):
+    cut = users.filter(Q(last_name__icontains=last_name))
+    if cut.count() > 0:
+        return cut
+    else:
+        cut = users.filter(Q(last_name__icontains=last_name[:3]))
+        if cut.count() > 0:
+            return cut
+        else:
+            cut = users.filter(Q(last_name__icontains=last_name[:2]))
+            if cut.count() > 0:
+                return cut
+            else:
+                cut = users.filter(Q(last_name__icontains=last_name[:1]))
+                if cut.count() > 0:
+                    return cut
+        return users
+
 def check_for_popup(user):
     if user != None and user.is_anonymous == False:
         if user.ask_for_claim:
-            shadows = User.objects.filter(first_name=str(user.first_name), unclaimed = True)
-            if shadows.count() > 15:
-                cut = shadows.filter(Q(last_name__icontains=str(user.last_name)[0]))
-                if cut.count() > 0:
-                    shadows = cut
-            elif shadows.count() == 0:
-                return None
+            users = User.objects.filter(first_name__icontains=str(user.first_name), unclaimed = True)
+
+            if users.count() > 4:
+                users = cut_last_name(users, str(user.last_name))
+            elif users.count() == 0:
+                users = User.objects.filter(first_name__icontains=str(user.first_name)[:5], unclaimed = True)
+                if users.count() > 4:
+                    users = cut_last_name(users, str(user.last_name))
+                elif users.count() == 0:
+                    return None
             shadow_list = []
-            for shadow in shadows:
+            for shadow in users[:4]:
                 shadow_list.append([str(shadow.first_name) + ' ' + str(shadow.last_name), get_location_basic_obj(shadow)])
-            return [shadow_list, False, True, False, False, False]
+
+            popup = Popup()
+            popup.popup_id = 2
+            popup.data = shadow_list
+            popup.data_length = len(shadow_list)
+            return popup
+
+
+
+
