b869d0a3e3e35a2204cca2b93df3f90c52c5c8ec
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-09-14 17:20:42 -0400

    Update views.py

--- scoreboard/views.py
+++ scoreboard/views.py
@@ -1,8 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from scoreboard.ranking import run_statistics
-
 
 def scoreboard(request):
-    return HttpResponse(run_statistics())
+    return HttpResponse('None')
