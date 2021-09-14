77513bad79d8d394ba7d88714166bf41bd96d1f5
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-09-14 13:09:53 -0400

    Automatic save commit initiated at 2021-09-14_13-09-53

--- accounts/management/commands/runstats.py
+++ accounts/management/commands/runstats.py
@@ -1,7 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
 
 from scoreboard.ranking import run_statistics
-from scraper import master_scrape
 
 
 class Command(BaseCommand):
