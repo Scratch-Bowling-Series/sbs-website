261b90f3acda74108be755383837220b8cd1dc75
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-09-19 17:41:27 -0400

    Automatic save commit initiated at 2021-09-19_17-41-27

--- accounts/management/commands/centers_length.py
+++ accounts/management/commands/centers_length.py
@@ -1,12 +1,21 @@
 from django.core.management.base import BaseCommand, CommandError
 
+from ScratchBowling.sbs_utils import is_valid_uuid
+from centers.center_utils import get_center
 from centers.models import Center
 from scoreboard.ranking import convert_tournaments
+from tournaments.models import Tournament
 
 
 class Command(BaseCommand):
     help = 'Run Statistics'
 
     def handle(self, *args, **options):
+        for tournament in Tournament.objects.all():
+            center_id = is_valid_uuid(tournament.center)
+            center = get_center(center_id)
+            if center != None:
+                print(str(center_id) + ' : ' + str(center.center_name))
+                break
         length = Center.objects.all().count()
         print(str(length))
