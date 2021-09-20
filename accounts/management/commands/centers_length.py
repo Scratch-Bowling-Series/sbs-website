0582f25447f406347bb56bd7c8790866209414df
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-09-19 22:12:19 -0400

    Automatic save commit initiated at 2021-09-19_22-12-19

--- accounts/management/commands/centers_length.py
+++ accounts/management/commands/centers_length.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 
 from ScratchBowling.sbs_utils import is_valid_uuid
+from ScratchBowling.views import User
 from centers.center_utils import get_center
 from centers.models import Center
 from scoreboard.ranking import convert_tournaments
@@ -13,12 +14,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
 
-        tournaments = Tournament.objects.all()
-        for tournament in tournaments:
-            center_id = is_valid_uuid(tournament.center)
-            center = get_center(center_id)
-            if center != None:
-                print(str(center_id) + ' : ' + str(center.center_name))
-                break
-            else:
-                print(str(tournament.center))
+        users = User.objects.all()
+        for user in users:
+            user.unclaimed = True
+            user.save()
