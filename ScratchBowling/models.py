672ec09d85aa91add56ba424aef55a21ff21e2ae
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2021-09-25 04:11:34 -0400

    Automatic save commit initiated at 2021-09-25_04-11-34

--- ScratchBowling/models.py
+++ ScratchBowling/models.py
@@ -3,12 +3,7 @@ import uuid
 from django.db import models
 
 
-class Homepage_Cache(models.Model):
-    cache_id = models.IntegerField(primary_key=True, default=0)
-    tournament_winners = models.BinaryField(blank=True, null=True)
-    top_ten_rankings = models.BinaryField(blank=True, null=True)
-    recent_tournament = models.BinaryField(blank=True, null=True)
-    bowler_of_month = models.BinaryField(blank=True, null=True)
+
 
 
 class Series(models.Model):
