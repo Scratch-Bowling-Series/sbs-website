f8236accd46cb36a024afc7ac49de0471c0372ec
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2022-01-09 11:50:29 -0500

    Update scrape.py

--- accounts/management/commands/scrape.py
+++ accounts/management/commands/scrape.py
@@ -1,10 +1,15 @@
+from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
 
-from scraper import master_scrape
+from centers.models import Center
+from tournaments.models import Tournament
 
+User = get_user_model()
 
 class Command(BaseCommand):
     help = 'Run Master Scraper'
 
     def handle(self, *args, **options):
-        master_scrape(True)
+        User.update_all(True)
+        Center.update_all(True)
+        Tournament.update_all(True)
