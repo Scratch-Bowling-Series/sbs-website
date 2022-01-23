46fd9cf6bc5813c67331752d5a1a8b834b073388
ChristianJStarr <56407604+ChristianJStarr@users.noreply.github.com>
2022-01-22 19:31:50 -0500

    Update scrape.py

--- accounts/management/commands/scrape.py
+++ accounts/management/commands/scrape.py
@@ -2,6 +2,8 @@ from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
 
 from centers.models import Center
+from oils.models import Oil_Pattern
+from oils.oil_pattern_scraper import update_library
 from tournaments.models import Tournament
 
 User = get_user_model()
@@ -10,10 +12,10 @@ class Command(BaseCommand):
     help = 'Run Master Scraper'
 
     def handle(self, *args, **options):
-        logging = args[0]
-        User.update_all(logging)
-        Center.update_all(logging)
-        Tournament.update_all(logging)
-        #Tournament.only_eat_all(logging)
-        Tournament.populate_datas(logging)
-
+        #User.update_all(True)
+        Center.update_all(True)
+        #Tournament.update_all(True)
+        #Tournament.only_eat_all(True)
+        #Tournament.populate_datas(True)
+        #update_library()
+        #print(Oil_Pattern.objects.all().count())
